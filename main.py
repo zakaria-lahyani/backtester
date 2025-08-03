24c088d48c083de12b61e1aa9501324344930fac
zakaria-lahyani <zakaria.lahyani.zli@gmail.com>
2025-08-03 14:05:57 +0200

    add config folder

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    """Simple main function"""
+    # Configuration
+    symbol = "xauusd"
+    indicator = "macd"
+    strategy_type = "simple"
+
+
+if __name__ == "__main__":
+    main()
